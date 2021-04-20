8687e1c0f54dc57e68cba8b3aa372c59d0c67181
RicentCB <ricardo.cb009@gmail.com>
2021-04-19 23:25:02 -0500

    [ADD] Funcion actualizar relojes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,29 @@
+var clockM;
+var clock1;
+var clock2;
+var clock3;
+const getRandom = maxNum=>(Math.floor(Math.random() * (maxNum+1)))
+
+const initClocks = ()=>{
+    clockM = new Clock(getRandom(24), getRandom(60), getRandom(60));
+    clock1 = new Clock(getRandom(24), getRandom(60), getRandom(60));
+    clock2 = new Clock(getRandom(24), getRandom(60), getRandom(60));
+    clock3 = new Clock(getRandom(24), getRandom(60), getRandom(60));
+}
 
 
 $(document).ready(function(){
-    console.log("Script")
+    //Inicializar Relojes
+    initClocks();
 
-    let clock1 = new Clock(24, 59, 10);
-    console.log(clock1);
-    for (let i = 0; i < 60; i++) {
-        clock1.update();
-    }
-    console.log(clock1);
+    console.log(clockM)
+    console.log(clock1)
+    console.log(clock2)
+    console.log(clock3)
+    
+    
+    // let interval1 = 1000;
+    // setInterval(clock1.update(), interval1);
+
+
 })
