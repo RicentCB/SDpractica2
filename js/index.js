453c458de05bf56cc129577ac3341a787f286f6b
RicentCB <ricardo.cb009@gmail.com>
2021-04-20 10:50:51 -0500

    [UP] Funcion para actualizar interfaz del reloj

--- js/index.js
+++ js/index.js
@@ -9,46 +9,40 @@ const asDoubleDigit = num=> {
         return num.toString();
 }
 
+const updateClockDom = (domElement, clock)=>{
+    domElement.find("h1.hours").html(asDoubleDigit(clock.getHours()));
+    domElement.find("h1.mins").html(asDoubleDigit(clock.getMinutes()));
+    domElement.find("h1.secs").html(asDoubleDigit(clock.getSeconds()));
+}
+
 var workerM = new Worker('./js/clock.js');
 var worker1 = new Worker('./js/clock.js');
 var worker2 = new Worker('./js/clock.js');
 var worker3 = new Worker('./js/clock.js');
 
+//Reloj Maestro
 workerM.onmessage = function (e) {
     let clockM = e.data;
     logClock('Master Clock', clockM);
-    //Reloj Maestro
-    let domClockM = $(".clock#clock-m");
-    domClockM.find("h1.hours").html(asDoubleDigit(clockM.getHours()));
-    domClockM.find("h1.mins").html(asDoubleDigit(clockM.getMinutes()));
-    domClockM.find("h1.secs").html(asDoubleDigit(clockM.getSeconds()));
+    updateClockDom($(".clock#clock-m"), clockM);
 }
+//Reloj 1
 worker1.onmessage = function (e) {
     let clock1 = e.data;
     logClock('Clock 1', clock1);
-    //Reloj 1
-    let domClock1 = $(".clock#clock-1");
-    domClock1.find("h1.hours").html(asDoubleDigit(clock1.getHours()));
-    domClock1.find("h1.mins").html(asDoubleDigit(clock1.getMinutes()));
-    domClock1.find("h1.secs").html(asDoubleDigit(clock1.getSeconds()));
+    updateClockDom($(".clock#clock-1"), clock1);
 }
+//Reloj 2
 worker2.onmessage = function (e) {
     let clock2 = e.data;
     logClock('Clock 2', clock2);
-    //Reloj 2
-    let domClock2 = $(".clock#clock-2");
-    domClock2.find("h1.hours").html(asDoubleDigit(clock2.getHours()));
-    domClock2.find("h1.mins").html(asDoubleDigit(clock2.getMinutes()));
-    domClock2.find("h1.secs").html(asDoubleDigit(clock2.getSeconds()));
+    updateClockDom($(".clock#clock-2"), clock2);
 }
+//Reloj 3
 worker3.onmessage = function (e) {
     let clock3 = e.data;
     logClock('Clock 3', clock3);
-    //Reloj 3
-    let domClock3 = $(".clock#clock-3");
-    domClock3.find("h1.hours").html(asDoubleDigit(clock3.getHours()));
-    domClock3.find("h1.mins").html(asDoubleDigit(clock3.getMinutes()));
-    domClock3.find("h1.secs").html(asDoubleDigit(clock3.getSeconds()));
+    updateClockDom($(".clock#clock-3"), clock3);
 }
 
 export default function main() {
